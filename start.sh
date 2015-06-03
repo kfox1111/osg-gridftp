d1fb15f847c0e49c49c9ce4fdd0712f9b4caba80
Kevin Fox <Kevin.Fox@pnnl.gov>
2015-06-03 16:55:29 -0700

    Make lcmaps.db symlink. fix log symlinks. make lcmaps log log things too.

--- start.sh
+++ start.sh
@@ -46,12 +46,17 @@ then
 else
 	mv /etc/gridftp.d/udt-osg-gridftp.conf /srv/globus/etc/udt-osg-gridftp.conf.newdisabled
 fi
+mkdir -p /etc/lcmaps
+ln -s /etc/lcmaps.db /etc/lcmaps/lcmaps.db
 rm -rf /etc/gridftp.d
 ln -s /srv/globus/etc/lcmaps.db /etc/lcmaps.db
 ln -s /srv/globus/etc/gridftp.conf /etc/gridftp.conf
 ln -s /srv/globus/etc/grid-security /etc/grid-security
 ln -s /srv/globus/etc/gridftp.d /etc/gridftp.d
-ln -s /srv/globus/var/log/globus-auth.log /var/log/globus-auth.log
-ln -s /srv/globus/var/log/globus.log /var/log/globus.log
+ln -s /srv/globus/var/log/gridftp-auth.log /var/log/gridftp-auth.log
+ln -s /srv/globus/var/log/gridftp.log /var/log/gridftp.log
+touch /srv/globus/var/log/gridftp-auth.log
+touch /srv/globus/var/log/gridftp.log
+export LCMAPS_LOG_FILE=/srv/globus/var/log/lcmaps
 /usr/sbin/globus-gridftp-server -c /etc/gridftp.conf -C /etc/gridftp.d -pidfile /var/run/globus-gridftp-server.pid
 
